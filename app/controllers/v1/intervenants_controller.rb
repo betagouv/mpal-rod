a30c0f8d9ce42dfa9f5837e61319ea87bb4ef928
Aryana Pezé <aryana.peze@octo.com>
2017-11-09 15:02:55 +0100

    mise à jour operation programmee

--- app/controllers/v1/intervenants_controller.rb
+++ app/controllers/v1/intervenants_controller.rb
@@ -14,7 +14,6 @@ class V1::IntervenantsController < ApplicationController
       departement = code_commune[0..1]
     end
 
-<<<<<<< HEAD
     if CODES_COMMUNE_OPA.include? code_commune
       return render plain: {
         "operation_programmee": OPERATIONS_PROGRAMMEES[departement],
@@ -29,26 +28,14 @@ class V1::IntervenantsController < ApplicationController
       return render plain: {
         "code_commune": code_commune,
         "type_departement": "Non déployé",
-        "operateur": OPERATEURS[departement],
+        "operateurs": OPERATEURS[departement],
         "service_instructeur": INSTRUCTEURS[departement][0],
         "dlc2": [],
         "pris_anah": PRIS[departement],
         "pris_eie": [],
       }.to_json
     end
-=======
-    return render text: {
-      "code_commune": code_commune,
-      "type_departement": "Non déployé",
-      "operateurs": OPERATEURS[departement],
-      "service_instructeur": INSTRUCTEURS[departement][0],
-      "dlc2": [],
-      "pris_anah": PRIS[departement],
-      "pris_eie": [],
-    }.to_json
->>>>>>> rajoute un S à operateurs
   end
-
 
 private
   def set_headers
